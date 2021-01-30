1573dac280ab237b67ef85f2c5dd2399bbd177ce
NHardie <60273209+NHardie@users.noreply.github.com>
2021-01-30 15:26:53 +0000

    Update settings.py

--- htf_web/settings.py
+++ htf_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n6sd7yrooaz+#v-47ncnfy8(2p-9x83^6wfx_gby6opj#nl_qw'
+SECRET_KEY = 'EXAMPLE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
