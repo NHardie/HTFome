0ca0ef85b5dc47bddc1fad9d0a04809eb2e7d9f3
NHardie <nhardie00@gmail.com>
2021-02-03 14:38:10 +0000

    Added Database functionality

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# Add models, aka databases, can now add things via the /admin page
+
+from .models import Htf, Drug
+
+admin.site.register(Htf)
+admin.site.register(Drug)
