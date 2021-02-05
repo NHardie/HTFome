d8aaf50d1d5588af78b29faf4bf3a62b033706c6
NHardie <nhardie00@gmail.com>
2021-02-05 13:47:57 +0000

    Added Search Functionality

--- htf_web/settings.py
+++ htf_web/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'EXAMPLE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'htf-app.eu-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['localhost', 'htf-app.eu-west-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
