fabfdacc6f846bd6f4bf0fa775e0587a6f4d656d
Alorax <tropa1996@gmail.com>
2021-02-23 20:49:45 +0000

    Update app.R

--- app.R
+++ app.R
@@ -153,9 +153,10 @@ server <- function(input, output) {
 
     # this reactive function takes user upload from the UI and passes it
     # to the read.delim() function, to read the data from the file.
-    # input$file1$datapath -> gives path of the file.
+    # input$file1$datapath -> is the data path contained in the value of
+    # the input that uploads the data.
     data <- reactive({
-        req(input$file1) # require input file to be available
+        req(input$file1) # require input file to be available before showing the output
         read.delim(input$file1$datapath)
     })
 
@@ -176,8 +177,7 @@ server <- function(input, output) {
     # argument to the getGEO function, which converts the file to a gds
     # object/data structure (with class GDS).
     gds <- reactive({
-        req(input$file1)
-        data()
+        req(data())
         getGEO(data(), GSEMatrix = TRUE)
         # getGEO(filename = input$file1$datapath, GSEMatrix = TRUE)
     })
@@ -197,6 +197,14 @@ server <- function(input, output) {
         gds()
     })
 
+
+    # # Checking data are uploaded before plotting etc
+    # output$map <- renderLeaflet({
+    #     if (is.null(data()) | is.null(map())) {
+    #         return(NULL)
+    #     }
+    #     ...
+    # })
 
     # Output ExpressionSet table
 
