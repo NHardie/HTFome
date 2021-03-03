a17e6d652b9e5fe277b92d1f2e40c2358de34965
alexanderma11 <72472179+alexanderma11@users.noreply.github.com>
2021-03-03 14:57:56 +0000

    Add this
    Script for GEO data summary

---
+++ GEO data summary script.R
@@ -0,0 +1,13 @@
+#script for gathering summary description of the GEO data
+
+desc <- Meta(gse)$description #get description of the data
+matrix_desc <- matrix(desc) #convert into matriix
+cur_desc <- list_desc[1] #extract only the actual description
+
+number_of_features <- Meta(gse)$feature_count #get the number of probes gathered in the experiment
+
+number_of_samples <- Meta(gse)$sample_count #get the number of samples
+
+organism <- Meta(gse)$sample_organism #get the sample organism
+
+sampletype <- Meta(gse)$sample_type #get the sample type
