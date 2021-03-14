ff8b68e11f76daa330553b8195902f03bd7f3011
NHardie <60273209+NHardie@users.noreply.github.com>
2021-03-14 11:11:37 +0000

    Update settings.py

--- htf_web/settings.py
+++ htf_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'EXAMPLE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'htf-app.eu-west-2.elasticbeanstalk.com', '127.0.0.1', 'htfome.com']
 
